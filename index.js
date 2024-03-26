de999efa44678e2df2877644ca3c5cf62be60273
PC School <appymrafli@gmail.com>
2024-03-26 08:06:03 +0700

    Revert "Add Auth, middleware and routes Auth"
    This reverts commit aacb8ac5526f2bfa402513ee944be9562931ad99.

--- index.js
+++ index.js
@@ -3,30 +3,20 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import fileUpload from 'express-fileupload';
 import session from 'express-session';
-import cookieParser from 'cookie-parser';
-import db from './config/Database.js';
-import SequelizeStore from 'connect-session-sequelize';
-import AuthRoute from './routes/AuthRoutes.js';
 import UserRoute from './routes/UsersRoutes.js';
 import ProductRoute from './routes/ProductRoutes.js';
 dotenv.config();
 
 const app = express();
 const port = process.env.APP_PORT || 5000;
-const sessionStore = SequelizeStore(session.Store);
-const store = new sessionStore({ db });
-// store.sync();
 
 app.use(
   session({
     secret: process.env.SESS_SECRET,
     resave: false,
     saveUninitialized: true,
-    store: store,
     cookie: {
       secure: 'auto',
-      // path: ''
-      // httpOnly: ''
     },
   })
 );
@@ -41,9 +31,7 @@ app.use(
 app.use(fileUpload());
 app.use(express.static('public'));
 app.use(express.json());
-app.use(cookieParser() /* untuk bisa memanipulasi sebuah cookie */);
 app.use(UserRoute);
 app.use(ProductRoute);
-app.use(AuthRoute);
 
 app.listen(port, () => console.log(`Running Server at Port: ${port}!`));
